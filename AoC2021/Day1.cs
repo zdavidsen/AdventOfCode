fd19732f337be0e76103eec6b0dbc0b37e49c17d
Zac <zdavidsen@gmail.com>
2021-12-01 00:28:56 -0500

    2021 day 1 done

--- AoC2021/Day1.cs
+++ AoC2021/Day1.cs
@@ -3,16 +3,42 @@
   [AoCChallenge(1, 2021)]
   public class Day1 : IAoCChallenge
   {
-    public bool Part2Ready => false;
+    public bool Part2Ready => true;
 
     public string RunPart1(IEnumerable<string> input)
     {
-      return "";
+      var last = int.Parse(input.ElementAt(0));
+      var count = 0;
+      foreach(var part in input.Skip(1))
+      {
+        var current = int.Parse(part);
+        if (current > last)
+          count++;
+        last = current;
+      }
+      return $"{count}";
     }
 
     public string RunPart2(IEnumerable<string> input)
     {
-      return "";
+      Queue<int> nums = new Queue<int>();
+      nums.Enqueue(0);
+      nums.Enqueue(0);
+      nums.Enqueue(0);
+      var sum = 0;
+
+      List<string> output = new List<string>();
+
+      foreach (var part in input)
+      {
+        var current = int.Parse(part);
+        sum += current;
+        nums.Enqueue(current);
+        sum -= nums.Dequeue();
+        output.Add($"{sum}");
+      }
+
+      return RunPart1(output.Skip(2));
     }
   }
 }
